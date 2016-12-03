7a85d7dab694558ffa1d3ea935a800cbdcd83b4b
Brandon <brandon@Brandons-MacBook-Pro.local>
2016-12-03 09:49:23 +0200

    jekyll webpage

---
+++ static/js/scroll.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 80
+    });
+
+    // Page scrolling feature
+    $('a.page-scroll').bind('click', function(event) {
+        var link = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(link.attr('href')).offset().top - 50
+        }, 500);
+        event.preventDefault();
+        $("#navbar").collapse('hide');
+    });
+});
+
+var cbpAnimatedHeader = (function() {
+    var changeHeaderOn = -1000, scrollDebounce = 250;
+    
+    function scrollY() {
+      return window.pageYOffset || document.documentElement.scrollTop;
+    }
+
+    window.addEventListener('scroll', function() { setTimeout(scrollPage, scrollDebounce); }, false);
+})();
+
+// Activate WOW.js plugin for animation on scroll
+new WOW().init();
